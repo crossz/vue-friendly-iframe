587b5f2c7a4051440d069ed921d4adb88c85195f
crossz <ukcrossa@gmail.com>
2021-07-15 17:25:52 +0800

    :tada: move <router-view /> out from the plugin and make it only for routes only for iframe.

--- src/components/VueRouterViewIframe/index.vue
+++ src/components/VueRouterViewIframe/index.vue
@@ -1,9 +1,9 @@
 <template>
     <div>
         <!-- Vue的router-view -->
-        <keep-alive>
+        <!-- <keep-alive> -->
             <router-view></router-view>
-        </keep-alive>
+        <!-- </keep-alive> -->
 
         <!-- iframe页 -->
         <component
